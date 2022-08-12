7223d9091f6a42f8a065efc6cb623704dd6192ec
DevilKo0l <nguyen.tcao96@gmail.com>
2022-08-12 22:26:38 +0200

    Update hero banner

--- frontend_next/pages/index.js
+++ frontend_next/pages/index.js
@@ -4,7 +4,7 @@ import { Product, FooterBanner, HeroBanner } from "../components";
 const index = () => {
   return (
     <>
-      HeroBanner{" "}
+      <HeroBanner />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
@@ -12,6 +12,7 @@ const index = () => {
       <div className="products-container">
         {["Product 1", "Product 2"].map((product) => product)}
       </div>
+      <FooterBanner />
     </>
   );
 };
